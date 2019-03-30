7456cd9ed6384a813c3987e6e44573f01a8f171a
AlphaArslan <arslan.alpha21@gmail.com>
2019-03-30 03:45:30 +0200

    needed for comunicatin on rpi side

---
+++ RPi_Code/Communication.py
@@ -0,0 +1,34 @@
+import socket
+import cv2
+import numpy as np
+
+def create_socket():
+	# Create a UDP socket
+	print("[INFO]:Creating Socket... ")
+	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	return sock
+
+def create_socket_receiving(port, host="127.0.0.1"):
+	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	sock.bind((host, port))
+	return sock
+
+def get_frames(host, port, sock):
+	print("[INFO]:Sending GET request...")
+	sent = sock.sendto("get".encode('ascii'), (host, port))
+
+	try:
+		answer, server = sock.recvfrom(65507)
+	except socket.timeout :
+		return get_frames(host, port, sock)
+
+	print("[INFO]:Frame Received")
+
+	array = np.frombuffer(answer, dtype=np.dtype('uint8'))
+	frame = cv2.imdecode(array, 1)
+
+	return frame
+
+def send_frame_bytes(frame_bytes, sock, port):
+	sock.sendto(frame_bytes, ("127.0.0.1", port))
