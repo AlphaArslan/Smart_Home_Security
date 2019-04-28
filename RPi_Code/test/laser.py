f96d20bbe6d8988b228e117297e40dbf4b971d53
AlphaArslan <arslan.alpha21@gmail.com>
2019-04-28 12:38:31 +0200

    dump typo

--- RPi_Code/test/laser.py
+++ RPi_Code/test/laser.py
@@ -6,11 +6,9 @@ LASER_PIN = 15          ## BCM
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(LASER_PIN, GPIO.OUT)
 
-try:
-    while True:
-        if GPIO.input(LASER_PIN) is False:
-            print("-- No Car Detected ")
-        else :
-            print("-- Car Detected ")
-        time.sleep(2)
-        
+while True:
+    if GPIO.input(LASER_PIN) is False:
+        print("-- No Car Detected ")
+    else :
+        print("-- Car Detected ")
+    time.sleep(2)
